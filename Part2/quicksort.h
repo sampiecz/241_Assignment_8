6fe62042cdae628d2bd6b848826a5fcf2c0002ef
Samuel Piecz <z1732715@turing.cs.niu.edu>
2017-11-20 12:36:23 -0600

    Part 1 is now working.  Sort.h was copied to Part 2.

--- Part2/quicksort.h
+++ Part2/quicksort.h
@@ -10,12 +10,12 @@ class Quicksort
     // Method prototypes
     public:
         void quickSort(vector<T>& set, bool (*compare)(const T&, const T&));
-        void quickSort(vector<T> & set, int start, int end, bool (*compare)(const T&, const T&))
-        int partition(vector<T>& set, int start, int end, bool (*compare)(const T&, const T&))
+        void quickSort(vector<T> & set, int start, int end, bool (*compare)(const T&, const T&));
+        int partition(vector<T>& set, int start, int end, bool (*compare)(const T&, const T&));
 };
 
 template <class T>
-void quickSort(vector<T>& set, bool (*compare)(const T&, const T&));
+void quickSort(vector<T>& set, bool (*compare)(const T&, const T&))
 {
     quickSort(set, 0, set.size()-1, compare);
 }
