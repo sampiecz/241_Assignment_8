70e7bb6e5bcb4e7d5965ba6391d46eaa7d7b6238
Samuel Piecz <z1732715@turing.cs.niu.edu>
2017-11-20 07:35:49 -0600

    Finished adding all pre provided code / logic. Next up start writing part 1.

--- Part2/quicksort.h
+++ Part2/quicksort.h
@@ -17,19 +17,47 @@ class Quicksort
 template <class T>
 void quickSort(vector<T>& set, bool (*compare)(const T&, const T&));
 {
-
+    quickSort(set, 0, set.size()-1, compare);
 }
 
 template <class T>
 void quickSort(vector<T> & set, int start, int end, bool (*compare)(const T&, const T&))
 {
+    int pivotPoint;
 
+    if (start < end)
+    {
+        pivotPoint = partition(set, start, end, compare);  // Get the pivot point
+        quickSort(set, start, pivotPoint - 1, compare);    // Sort first sublist
+        quickSort(set, pivotPoint + 1, end, compare);      // Sort second sublist
+    }
 }
 
 template <class T>
 int partition(vector<T>& set, int start, int end, bool (*compare)(const T&, const T&))
 {
+    int pivotIndex, mid;
+    T pivotValue;
 
+    mid = (start + end) / 2;
+
+    // Swap elements start and mid of the vector
+
+    pivotIndex = start;
+    pivotValue = set[start];
+
+    for (int scan = start + 1; scan <= end; scan++)
+    {
+        if (compare(set[scan], pivotValue))
+        {
+            pivotIndex++;
+            // Swap elements pivotIndex and scan of the vector
+        }
+    }
+
+    // Swap elements start and pivotIndex of the vector
+
+    return pivotIndex;
 }
 
 #endif
--- Part3/mergesort.h
+++ Part3/mergesort.h
@@ -20,16 +20,82 @@ void mergeSort(vector<T>& set, bool (*compare)(const T&, const T&))
 
 }
 
+// This recursive function divides a vector into two subvectors, sorts them, and then merges the two sorted subvectors.
 template <class T>
-void mergeSort(vecotr<T>& set, int low, int high, bool (*compare)(const T&, const T&))
+void mergeSort(vector<T>& set, int low, int high, bool (*compare)(const T&, const T&))
 {
+    int mid;
+    if (low < high)
+    {
+        mid = (low + high) / 2;
 
+        // Divide and conquer
+        
+        mergeSort(set, low, mid, compare);
+        mergeSort(set, mid+1, high, compare);
+
+        // Combine 
+        merge(set, low, mid, high, compare);
+    }
 }
 
+// This function merges two sorted subvectors.
 template <class T>
 void merge(vector<T>& set, int low, int mid, int high, bool (*compare)(const T&, const T&))
 {
+    int i = low;    // Subscript for start of left sorted subvector
+    int j = mid+1;  // Subscript for start of right sorted subvector
+    int k = 0;      // Subscript for start of merged vector
 
+    // While not at the end of either subvector
+    while (i <= mid && j <= high)
+    {
+        if (compare(set[j], set[i]))
+        {
+            /*
+            Copy elelment j of set into element k of temp
+            Add one to j
+            Add one to k
+            */
+        }
+        else
+        {
+            /*
+             * Copy element i of set into element k of temp
+             * Add one to i
+             * Add one to k
+             *
+             */
+        }
+    }
+
+    // Copy over any remaining elements of left subvector
+    while (i <= mid)
+    {
+        /*
+         * Copy element i of set into element k of temp
+         * Add one to i
+         * Add one to k
+         *
+         */
+    }
+
+    // Copy over any remaining elements of right subvector
+    while (j <= high)
+    {
+        /*
+         * Copy element j of set into element k of temp
+         * Add one to j
+         * Add one to k
+         *
+         */
+    }
+
+    // Copy merged elements back into original vector
+    for (i = 0, j = low; j <= high; i++, j++)
+    {
+        // Copy element i of tem into element j of set
+    }
 }
 
 
