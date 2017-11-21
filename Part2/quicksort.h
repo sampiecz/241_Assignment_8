eb909ee416a25bda2e0ed48b9fb351f38678f337
Samuel Piecz <z1732715@turing.cs.niu.edu>
2017-11-20 18:53:22 -0600

    Updated makefile. Tried to add compare pointer to function. Confused on that one at the moment.

--- Part2/quicksort.h
+++ Part2/quicksort.h
@@ -1,6 +1,9 @@
 #ifndef QUICKSORT_H
 #define QUICKSORT_H
 
+#include <cstring>
+using namespace std;
+
 template <class T>
 class Quicksort
 {
@@ -12,7 +15,21 @@ class Quicksort
         void quickSort(vector<T>& set, bool (*compare)(const T&, const T&));
         void quickSort(vector<T> & set, int start, int end, bool (*compare)(const T&, const T&));
         int partition(vector<T>& set, int start, int end, bool (*compare)(const T&, const T&));
+        bool (*compare)(const T&, const T&);
 };
+
+template <class T>
+bool (*compare)(const T& a, const T& b)
+{
+    if (!strcmp(a, b))
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
 
 template <class T>
 void quickSort(vector<T>& set, bool (*compare)(const T&, const T&))
@@ -59,5 +76,7 @@ int partition(vector<T>& set, int start, int end, bool (*compare)(const T&, cons
 
     return pivotIndex;
 }
+
+bool (*compare)(int a, int b);
 
 #endif
